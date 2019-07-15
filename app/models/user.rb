65f29a9136b621a00c6fe95c5b1b23afc5ca3dad
Jack <superjackpower85@gmail.com>
2019-07-15 12:52:43 +0800

    second commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,13 @@
 class User < ActiveRecord::Base
   validates_presence_of :email
   has_many :posts
-
   include BCrypt
 
   def password
     @password ||= Password.new(password_hash)
   end
-
   def password=(new_password)
     @password =Password.create(new_password)
     self.password_hash = @password
   end
-  
 end
--- config/environment.rb
+++ config/environment.rb
@@ -1,11 +1,9 @@
 require 'bundler/setup'
 Bundler.require
-
 ENV['SINATRA_ENV'] ||= "development"
 
 unless ENV['RACK_ENV'] == 'production'
 require 'dotenv'
 Dotenv.load
 end
-
 require_all 'app'
